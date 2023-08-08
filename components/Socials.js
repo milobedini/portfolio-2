ecaf780d80bb2b1ac71c00448adc8a41ba029231
Milo Bedini <milobedini64@gmail.com>
2023-08-08 14:04:03 +0100

    header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,20 @@
-const Socials = () => {
-  return <div>Socials</div>;
-};
+import Link from 'next/link'
+import { RiFacebookLine, RiLinkedinLine, RiGithubLine } from 'react-icons/ri'
 
-export default Socials;
+const Socials = () => {
+  return (
+    <div className="flex items-center gap-x-5 text-3xl">
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+    </div>
+  )
+}
+
+export default Socials
