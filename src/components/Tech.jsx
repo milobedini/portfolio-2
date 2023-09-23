9c034d98b530ea21de278e82944e4aa46c91534f
Milo Bedini <milobedini64@gmail.com>
2023-09-23 18:23:06 +0100

    tech labels

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -11,9 +11,16 @@ const Tech = () => {
         <motion.div
           variants={fadeIn('up', 'spring', index * 0.15, 0.75)}
           key={index}
-          className="text-6xl text-white hover:text-accent"
+          className="group relative text-6xl text-white hover:text-accent"
         >
           {tech}
+          <div
+            className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full
+           bg-black bg-opacity-70 text-accent px-2 py-1 rounded mt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300
+           font-medium text-center capitalize text-base"
+          >
+            {tech.key}
+          </div>
         </motion.div>
       ))}
     </div>
